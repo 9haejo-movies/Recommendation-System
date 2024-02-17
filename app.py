e2b7b40ba4fddb0f59e431cb974e33aabd334863
GaGyeong-Kim <108977032+GaGyeong-Kim@users.noreply.github.com>
2024-02-17 16:51:21 +0900

    sorting popularity, released date

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import pickle
 import streamlit as st
-import pandas as pd
-import numpy as np
 from tmdbv3api import Movie, TMDb
+
+def get_movie_details(movie_id, api_key='c24a5b64e7186cfcf4a4b840c3138e12'):
+    movie = Movie()
+    tmdb = TMDb()
+    tmdb.api_key = api_key
+    details = movie.details(movie_id)
+    return details
 
 def get_recommendations_pop(title):
     idx = movies[movies['title'] == title].index[0]
@@ -16,20 +21,22 @@ def get_recommendations_pop(title):
     images = []
     titles = []
     genres = []
+    ids = []
     
     for movie_index in recommended_movies.index:
         movie_id = movies['id'].iloc[movie_index]
-        details = movie.details(movie_id)
-        image_path = details['poster_path']
+        details = get_movie_details(movie_id, 'c24a5b64e7186cfcf4a4b840c3138e12')
+        image_path = details.get('poster_path')
         if image_path:
             image_path = 'https://image.tmdb.org/t/p/w500' + image_path
         else: 
             image_path = 'no_image.jpg'
         images.append(image_path)
-        titles.append(details.title)
-        genres.append(details.genres)
+        titles.append(details.get('title'))
+        genres.append([genre['name'] for genre in details.get('genres', [])])  # Extract all genres
+        ids.append(movie_id)
     
-    return images, titles, genres
+    return images, titles, genres, ids
 
 def get_recommendations_year(title):
     idx = movies[movies['title'] == title].index[0]
@@ -42,70 +49,49 @@ def get_recommendations_year(title):
     images = []
     titles = []
     genres = []
+    ids = []
     
     for movie_index in recommended_movies.index:
         movie_id = movies['id'].iloc[movie_index]
-        details = movie.details(movie_id)
-        image_path = details['poster_path']
+        details = get_movie_details(movie_id, 'c24a5b64e7186cfcf4a4b840c3138e12')
+        image_path = details.get('poster_path')
         if image_path:
             image_path = 'https://image.tmdb.org/t/p/w500' + image_path
         else: 
             image_path = 'no_image.jpg'
         images.append(image_path)
-        titles.append(details.title)
-        genres.append(details.genres)
+        titles.append(details.get('title'))
+        genres.append([genre['name'] for genre in details.get('genres', [])])  # Extract all genres
+        ids.append(movie_id)
     
-    return images, titles, genres
+    return images, titles, genres, ids
 
-movie = Movie()
-tmdb = TMDb()
-tmdb.api_key = ''
-#tmdb.language = 'ko-KR'
-
-# cosine similarity load
+# Load movies and cosine similarity matrix
 movies = pickle.load(open('movies_final.pickle','rb'))
 cosine_sim = pickle.load(open('cosine_sim_final.pickle','rb'))
 
 st.set_page_config(layout='wide')
-st.header('🎥Movie Recommendation System🎥')
+st.header('🎥 Movie Recommendation System 🎥')
 
-movie_list = movies['title'].values
-
-title = st.selectbox('Choose a movie you like', movie_list)
-
+title = st.selectbox('Choose a movie you like', movies['title'].values)
 sorting_method = st.radio("Select sorting method:", ('Popular', 'Latest'))
 
 if st.button('Recommend'):
     with st.spinner('Wait for it...'):
-        if sorting_method == 'Popularity':
-            images, titles, genres = get_recommendations_pop(title)
+        if sorting_method == 'Popular':
+            images, titles, genres, ids = get_recommendations_pop(title)
         else:
-            images, titles, genres = get_recommendations_year(title)
-
-        tab_titles = ['추천 영화 10개 목록', '영화 정보']
-        tabs = st.tabs(tab_titles)
-
-        with tabs[0]:
-            idx = 0
-            for i in range(0,2):
-                cols = st.columns(5)
-                for j in range(0,5):
-                    cols[j].image(images[idx], width=100, use_column_width=True, caption=titles[idx])
-                    checkbox_key = f"checkbox_{idx}"
-                    if cols[j].write("",key=checkbox_key):
-                        cols[j].image(images[idx], width=300, use_column_width=True, caption=titles[idx])
-                    idx += 1
+            images, titles, genres, ids = get_recommendations_year(title)
         
-        with tabs[1]:
-            idx = 0
-            for i in range(0,2):
-                cols = st.columns(5)
-                for j in range(0,5):
-                    cols[j].image(images[idx], width=150)
-                    cols[j].write(f'✔️제목 : {titles[idx]}')
-                    genre_names = ', '.join([genre['name'] for genre in genres[idx]])
-                    cols[j].write(f'✔️장르 : {genre_names}')
-                    cols[j].write(f'✔️개봉연도 : {movies["release_date"][idx]}')
-                    idx += 1         
-
-
+        idx = 0
+        for i in range(0, 2):
+            cols = st.columns(5)
+            for j in range(0, 5):
+                # Display the image with column width
+                cols[j].image(images[idx], use_column_width=True, output_format='JPEG')
+                
+                # Display title and genres next to the image
+                cols[j].write("**✔️[Title]** " + titles[idx])
+                cols[j].write("**✔️[Genres]** " + ", ".join(genres[idx]))  # Display all genres
+                
+                idx += 1
